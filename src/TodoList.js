1440d83cf299165a6678feda8be7f6dbb84553d8
Rahul Vats <vats.rahul@gmail.com>
2021-05-04 14:22:32 -0500

    Updating Todo List Code and getting the length to render on the displayed page

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function TodoList(todos) {
+export default function TodoList({todos}) {
   return (
     <div>
       <h1>To Do List</h1>
-      { todos.Length }
+      {todos.length}
     </div>
   )
 }
